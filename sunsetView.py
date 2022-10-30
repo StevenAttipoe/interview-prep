02ab9fabab9bb08cc872766f4f08be3db1385251
Steven Attipoe <stevenattipoe7@gmail.com>
2022-10-30 10:50:21 +0000

    added test to sunsetView

--- sunsetView.py
+++ sunsetView.py
@@ -17,3 +17,42 @@ def sunsetViews(buildings, direction):
                 maxValue = buildings[i]
                 candidateBuildings.append(i)
         return candidateBuildings
+
+def sunsetViews2(buildings, direction):
+    candidateBuildings = []
+    maxValue = 0
+    
+    for i in range(len(buildings) -1 , -1, -1) if direction == "EAST" else range(len(buildings)):
+        if buildings[i] > maxValue:
+                maxValue = buildings[i]
+                candidateBuildings.append(i)
+    
+    return candidateBuildings[::-1] if direction == "EAST" else candidateBuildings
+    
+def test_east_sunsetView():
+        buildings = [3, 5, 4, 4, 3, 1, 3, 2]
+        direction = "EAST"
+        expected = [1, 3, 6, 7]
+        actual = sunsetViews(buildings, direction)
+        assert(actual == expected)
+
+def test_east_sunsetView2():
+        buildings = [3, 5, 4, 4, 3, 1, 3, 2]
+        direction = "EAST"
+        expected = [1, 3, 6, 7]
+        actual = sunsetViews2(buildings, direction)
+        assert(actual == expected)
+
+def test_west_sunsetView():
+        buildings = [3, 5, 4, 4, 3, 1, 3, 2]
+        direction = "WEST"
+        expected = [0, 1]
+        actual = sunsetViews(buildings, direction)
+        assert(actual == expected)
+
+def test_west_sunsetView2():
+        buildings = [3, 5, 4, 4, 3, 1, 3, 2]
+        direction = "WEST"
+        expected = [0, 1]
+        actual = sunsetViews2(buildings, direction)
+        assert(actual == expected)
