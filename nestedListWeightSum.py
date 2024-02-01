661720c4d8a22eb1202c3f3802ea227df3a3a2ed
StevenAttipoe <stevenattipoe7@gmail.com>
2024-02-01 14:39:22 +0000

    solved nestedListWeightSum

---
+++ nestedListWeightSum.py
@@ -0,0 +1,18 @@
+def nestedListWeightSum(nestedList) -> int:
+    def dfs(nestedList, depth):
+        weightSum = 0
+        for element in nestedList:
+            if type(element) == list:
+                weightSum += dfs(element, depth + 1)
+            else:
+                weightSum += int(element) * depth
+        return weightSum
+    return dfs(nestedList, 1)
+
+
+result = depthSum([[1,1],2,[1,1]])
+assert(result == 10)
+
+
+result = depthSum([1,[4,[6]]])
+assert(result == 27)
